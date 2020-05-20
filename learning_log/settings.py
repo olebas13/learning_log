31aea4c714a6df0e3f63a9aaa9d8150230949f4b
olebas13 <olebas13@gmail.com>
2020-05-20 15:32:22 +0300

    set debug=false for heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -140,8 +140,10 @@ if os.getcwd() == '/app':
     # Поддержка заголовка 'X-Forwarded-Proto' для request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Разрешены все заголовки хостов
-    ALLOWED_HOSTS = ['*']
+    # Хостом проекта может быть только Heroku
+    ALLOWED_HOSTS = ['olebas13.herokuapp.com']
+
+    DEBUG = False
 
     # Конфигурация статических ресурсов
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
